5614ed4abaa9d2caa1f3d82ccc9ea9fc5783f534
yiplee <guoyinl@gmail.com>
2019-11-21 18:54:03 +0800

    [http/request] add localizer

--- http/request/context.go
+++ http/request/context.go
@@ -4,7 +4,12 @@ import (
 	"io/ioutil"
 	"strings"
 
+	"github.com/fox-one/pkg/text/localizer"
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	localizerContextKey = "localizer_context_key"
 )
 
 func Body(c *gin.Context) (body []byte, err error) {
@@ -31,4 +36,14 @@ func Token(c *gin.Context) string {
 
 func AcceptLanguage(c *gin.Context) string {
 	return c.GetHeader("Accept-Language")
+}
+
+func WithLocalizer(l *localizer.Localizer) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set(localizerContextKey, localizer.WithLanguage(l, AcceptLanguage(c)))
+	}
+}
+
+func Localizer(c *gin.Context) *localizer.Localizer {
+	return c.MustGet(localizerContextKey).(*localizer.Localizer)
 }
