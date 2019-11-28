4bd9d69e699b673a5342811fe0ce2f9dc6cb84b2
yiplee <guoyinl@gmail.com>
2019-11-28 15:15:32 +0800

    add pkg: efoxapi

---
+++ efoxapi/market.go
@@ -0,0 +1,73 @@
+package foxapi
+
+import (
+	"context"
+
+	"github.com/shopspring/decimal"
+)
+
+type (
+	Market struct {
+		Depth  *Depth   `json:"depth,omitempty"`
+		Trades []*Trade `json:"trades,omitempty"`
+		Ticker *Ticker  `json:"ticker,omitempty"`
+		Pair   *Pair    `json:"pair,omitempty"`
+	}
+
+	DepthOrder [2]decimal.Decimal
+	Depth      struct {
+		Version string       `json:"version,omitempty"`
+		Asks    []DepthOrder `json:"asks,omitempty"`
+		Bids    []DepthOrder `json:"bids,omitempty"`
+	}
+
+	Trade struct {
+		ID        string          `json:"id,omitempty"`
+		Timestamp int64           `json:"time,omitempty"`
+		Price     decimal.Decimal `json:"price,omitempty"`
+		Amount    decimal.Decimal `json:"amount,omitempty"`
+		Side      string          `json:"side,omitempty"`
+	}
+
+	Ticker struct {
+		Last   decimal.Decimal `json:"last,omitempty"`
+		Change decimal.Decimal `json:"change,omitempty"`
+		Buy    decimal.Decimal `json:"buy,omitempty"`
+		Sell   decimal.Decimal `json:"sell,omitempty"`
+		High   decimal.Decimal `json:"high,omitempty"`
+		Low    decimal.Decimal `json:"low,omitempty"`
+		Volume decimal.Decimal `json:"volume,omitempty"`
+	}
+
+	Asset struct {
+		AssetID   string                     `json:"asset_id,omitempty"`
+		Symbol    string                     `json:"symbol,omitempty"`
+		Min       decimal.Decimal            `json:"min,omitempty"`
+		Minimums  map[string]decimal.Decimal `json:"minimums,omitempty"`
+		Max       decimal.Decimal            `json:"max,omitempty"`
+		Precision int                        `json:"precision,omitempty"`
+	}
+
+	Pair struct {
+		Base           *Asset `json:"base,omitempty"`
+		Quote          *Asset `json:"quote,omitempty"`
+		CanBuy         bool   `json:"can_buy,omitempty"`
+		CanSell        bool   `json:"can_sell,omitempty"`
+		Symbol         string `json:"symbol,omitempty"`
+		PricePrecision int    `json:"price_precision,omitempty"`
+	}
+)
+
+func ReadMarket(ctx context.Context, symbol string) (*Market, error) {
+	resp, err := request(ctx).SetQueryParam("symbol", symbol).Get("/market")
+	if err != nil {
+		return nil, err
+	}
+
+	var market Market
+	if err := decodeResponse(resp, &market); err != nil {
+		return nil, err
+	}
+
+	return &market, nil
+}
---
+++ efoxapi/market_test.go
@@ -0,0 +1,17 @@
+package foxapi
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadMarket(t *testing.T) {
+	market, err := ReadMarket(context.Background(), "BTCUSDT")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "BTCUSDT", market.Pair.Symbol)
+		assert.NotEmpty(t, market.Trades)
+		assert.True(t, market.Ticker.Last.IsPositive())
+	}
+}
