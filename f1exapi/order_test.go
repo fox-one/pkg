f0eff79ea187e68067161314e947e86a3e1808ff
yiplee <guoyinl@gmail.com>
2019-12-22 02:10:33 +0800

    mod: _

--- f1exapi/order_test.go
+++ f1exapi/order_test.go
@@ -9,12 +9,8 @@ import (
 )
 
 const (
-	merchantID  = "aa6911ca-5fe6-3061-97fe-59fa2034e420"
-	merchantKey = `-----BEGIN RSA PRIVATE KEY-----
-MHcCAQEEIK8E20km9juc+sza6ZKjeH5xcfTMdads4O70jjjtmJAvoAoGCCqGSM49
-AwEHoUQDQgAEFB6V7yLFtyYNbo6MgC1Ljsxl6ag8EaSLKYdTRgCe7ygUuq3S1GJb
-F07WMTxb/27tLZlEy09hRx842FTguLheXA==
------END RSA PRIVATE KEY-----`
+	merchantID  = ""
+	merchantKey = ``
 )
 
 func TestQueryOrders(t *testing.T) {
