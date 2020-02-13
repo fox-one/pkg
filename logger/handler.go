f2b51c1e318e82b6dba49a52085b80e0b5475fe2
yiplee <guoyinl@gmail.com>
2020-02-13 14:05:03 +0800

    [logger] add log middleware

--- logger/handler.go
+++ logger/handler.go
@@ -48,3 +48,26 @@ func Handler() gin.HandlerFunc {
 		}
 	}
 }
+
+// Middleware provides logging middleware.
+func Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := r.Header.Get("X-Request-ID")
+		if id == "" {
+			id = uuid.New()
+		}
+		ctx := r.Context()
+		log := FromContext(ctx).WithField("request-id", id)
+		ctx = WithContext(ctx, log)
+		start := time.Now()
+		next.ServeHTTP(w, r.WithContext(ctx))
+		end := time.Now()
+		log.WithFields(logrus.Fields{
+			"method":  r.Method,
+			"request": r.RequestURI,
+			"remote":  r.RemoteAddr,
+			"latency": end.Sub(start),
+			"time":    end.Format(time.RFC3339),
+		}).Debug()
+	})
+}
