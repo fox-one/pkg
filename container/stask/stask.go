795a3f5156e85cec8ed640d15882c795dbbb5581
李国银 <guoyinl@gmail.com>
2020-04-10 11:23:24 +0800

    container: stask (#5)

---
+++ container/stask/stask.go
@@ -0,0 +1,41 @@
+package stack
+
+import "container/list"
+
+type Stack struct {
+	list *list.List
+}
+
+func New() *Stack {
+	return &Stack{list.New()}
+}
+
+func (stack *Stack) Push(value interface{}) {
+	stack.list.PushBack(value)
+}
+
+func (stack *Stack) Pop() interface{} {
+	e := stack.list.Back()
+	if e != nil {
+		stack.list.Remove(e)
+		return e.Value
+	}
+	return nil
+}
+
+func (stack *Stack) Peak() interface{} {
+	e := stack.list.Back()
+	if e != nil {
+		return e.Value
+	}
+
+	return nil
+}
+
+func (stack *Stack) Len() int {
+	return stack.list.Len()
+}
+
+func (stack *Stack) IsEmpty() bool {
+	return stack.list.Len() == 0
+}
