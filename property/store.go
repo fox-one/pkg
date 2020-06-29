a8f7dfa6967b7704c66dec0fa5204ef5969b4b3a
cw35 <ys.wu35@gmail.com>
2020-06-29 10:28:25 +0800

    MOD: property add expire function; (#7)
    Co-authored-by: Charlie Wu <wu@fox.one>

--- property/store.go
+++ property/store.go
@@ -5,5 +5,6 @@ import "context"
 type Store interface {
 	Get(ctx context.Context, key string) (Value, error)
 	Save(ctx context.Context, key string, value interface{}) error
+	Expire(ctx context.Context, key string) error
 	List(ctx context.Context) (map[string]Value, error)
 }
